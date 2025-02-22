02f0e6d962456505c87845a32a94c44eb492ea75
babarot <b4b4r07@gmail.com>
2025-02-22 19:23:51 +0900

    chore: add compiled action code

--- dist/check-changelog.d.ts
+++ dist/check-changelog.d.ts
@@ -1,10 +1,4 @@
-import { getOctokit } from '@actions/github';
-import { Context } from '@actions/github/lib/context';
-import type * as coreType from '@actions/core';
 interface CheckChangelogOptions {
-    github: ReturnType<typeof getOctokit>;
-    context: Context;
-    core: typeof coreType;
     baseSha: string;
     headSha: string;
 }
